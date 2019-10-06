7e2eaced91b9fdf978855aba0c032fe5d5eb8b88
Marvin.Min <marvin.min@activenetwork.com>
2019-10-06 21:04:38 +0800

    wrap layout

--- lib/flex_demo.dart
+++ lib/flex_demo.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FlexDemo extends StatelessWidget{
-
+class FlexDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Flex(
@@ -23,10 +22,15 @@ class FlexDemo extends StatelessWidget{
           child: Container(
 //            height: 100%,
             color: Colors.green,
+            child: Wrap(
+//          spacing: 8.0, // 主轴(水平)方向间距
+//          runSpacing: 4.0, // 纵轴（垂直）方向间距
+              alignment: WrapAlignment.start, //沿主轴方向居中
+              children: <Widget>[Text("hello" * 300)],
+            ),
           ),
         ),
       ],
     );
   }
-
 }
