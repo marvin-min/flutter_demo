c9e1da0fcdbc4daaf4cd6ffd542e0d528c686426
Marvin.Min <marvin.min@activenetwork.com>
2019-10-07 21:01:47 +0800

    add title

--- lib/list_view_demo.dart
+++ lib/list_view_demo.dart
@@ -5,6 +5,7 @@ class ListViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("hello"),),
       body: ListView1(),
     );
   }
@@ -13,13 +14,16 @@ class ListViewDemo extends StatelessWidget {
 class ListView1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
-      ListView.builder(
-          itemCount: 100,
-          itemExtent: 50.0,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(title: Text("$index"));
-          });
+    return Column(children: <Widget>[
+      ListTile(title: Text("商品列表")),
+      Expanded(
+          child: ListView.builder(
+              itemCount: 100,
+              itemExtent: 30.0,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(title: Text("$index"));
+              }))
+    ]);
   }
 }
 
